2157e4291ba571d48eade39dfb146fac362eae7c
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-01-09 00:10:44 +0200

    Adding asynchronous information to the database
    Linking two tables by similarity between title and name_of_tools

--- database/allocation_parsing.sql
+++ database/allocation_parsing.sql
@@ -3,11 +3,25 @@ CREATE DATABASE allocation_parsing;
 USE allocation_parsing;
 
 CREATE TABLE
-    Vault (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        link VARCHAR(255),
-        header VARCHAR(255),
-        download_link VARCHAR(255),
-        official_link VARCHAR(255),
-        official_image_links VARCHAR(225)
+    Vfxmed (
+        link VARCHAR(300) NOT NULL,
+        title VARCHAR(300) NOT NULL,
+        download_link VARCHAR(300) NOT NULL
     );
+
+CREATE TABLE
+    BlenderMarket(
+        off_link VARCHAR(300) NOT NULL,
+        name_of_tools VARCHAR(300) NOT NULL,
+        url_on_image VARCHAR(1000) NOT NULL
+    );
+
+SELECT
+    v.link,
+    v.title,
+    v.download_link,
+    b.off_link,
+    b.name_of_tools,
+    b.url_on_image
+FROM Vfxmed AS v
+    LEFT JOIN BlenderMarket AS b ON v.title LIKE CONCAT('%', b.name_of_tools, '%')
