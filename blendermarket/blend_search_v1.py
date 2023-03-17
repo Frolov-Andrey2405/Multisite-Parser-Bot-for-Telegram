df783e7ee8034f25a5d4a4eaabab42e11241b06d
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-03-17 23:47:26 +0200

    Single dictionary search

---
+++ blendermarket/blend_search_v1.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import Levenshtein
+
+FORBIDDEN_SYMBOLS = ('\\\\', '/', ':', '*', '?', '', '<', '>', '|', ' ')
+BASE_URL = "https://blendermarket.com/products"
+
+# Read vfxmed.json and extract the title
+with open('vfxmed/json/vfxmed.json', 'r') as f:
+    data = json.load(f)
+    title = data['title']
+
+# Define the threshold for Levenshtein distance
+threshold = 3
+
+# Iterate through all pages on blendermarket.com
+for page in range(1, 10):
+    url = BASE_URL + f'?page={page}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Find all card-body divs and extract the title
+    for div in soup.find_all('div', class_='card-body'):
+        card_title = div.find('h5', class_='card-title').text.strip()
+
+        # Calculate the Levenshtein distance between the titles
+        distance = Levenshtein.distance(title, card_title)
+
+        # If the distance is below the threshold, print the link
+        if distance <= threshold:
+            link = div.find('a', href=True)['href']
+            link = f'https://blendermarket.com/{link}'
+            print(link)
